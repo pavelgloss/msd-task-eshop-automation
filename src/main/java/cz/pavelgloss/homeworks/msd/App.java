0749a7c0e9bb766bd927aa76e9ba031640041d40
pgloss <pavel.gloss@post.cz>
2020-08-23 19:34:40 +0200

    selenium project skeleton

---
+++ src/main/java/cz/pavelgloss/homeworks/msd/App.java
@@ -0,0 +1,13 @@
+package cz.pavelgloss.homeworks.msd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
